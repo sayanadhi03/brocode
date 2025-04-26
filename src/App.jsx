573f3bc245f1d818bfa6fd506d0a564b98acf607
sayanadhi03 <timepasswithsa@gmail.com>
2025-04-26 18:47:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './header.jsx';
+import Footer from './Footer.jsx';
+import Food from './Food.jsx';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Food/>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return(
+        <header>
+            <h1>welcome to react</h1>
+            <p>my name is Sayan Adhikary</p>
+            <nav>
+                <ul>
+                    <li><a href="#"> Home</a></li>
+                    <li><a href="#"> About</a></li>
+                    <li><a href="#"> Services</a></li>
+                    <li><a href="#"> Contact</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    );
+}
+
+export default Header
